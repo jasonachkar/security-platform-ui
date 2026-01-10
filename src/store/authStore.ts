2230502cba4b95257950e1094f189675dc15e5ad
jasonachkar <jasonachkardiab@gmail.com>
2026-01-09 20:01:35 -0500

    initial UI design and cards

---
+++ src/store/authStore.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand';
+
+interface User {
+  id: number;
+  username: string;
+  role: string;
+}
+
+interface AuthState {
+  user: User | null;
+  token: string | null;
+  setAuth: (token: string, user: User) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  token: localStorage.getItem('token'),
+  setAuth: (token, user) => {
+    localStorage.setItem('token', token);
+    set({ token, user });
+  },
+  logout: () => {
+    localStorage.removeItem('token');
+    set({ token: null, user: null });
+  },
+}));
